e657643cc4c3032c4fd458a0c8b0581b9ce7c9cd
Walter Gerardo Gonzalez Sanchez <wgonzalez@credicorpcapital.com>
2023-09-22 10:20:41 -0500

    Aprendiendo mas de globales.

---
+++ modulos/globales.js
@@ -0,0 +1,16 @@
+// let i = 0;
+// let interval = setInterval(() => {
+//     console.log("Hola", i);
+//     if (i === 3) {
+//         clearInterval(interval);
+//     }
+//     i++;
+// }, 1000);
+
+// setImmediate(() => {
+//     console.log('Hola');
+// })
+
+global.miVariable = 'Mi variable.';
+
+console.log('Mi variable dice est: ', miVariable);
